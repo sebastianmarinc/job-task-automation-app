69563a94412814ffe2e0901d1dc87910a876e315
Sebastian Marin <smarin@paloaltonetworks.com>
2025-06-06 15:33:33 -0700

    updating default values

--- app.py
+++ app.py
@@ -171,12 +171,7 @@ st.sidebar.info(f"Current minimum match strength: {selected_similarity_threshold
 default_job_tasks = """Develop narratives and materials that clearly communicate the value of our unified management solution and Digital Experience Management capabilities, translating technical features into compelling messages that resonate with customers
 Create impactful marketing content, including datasheets, whitepapers, blogs, and presentations
 Define and implement GTM strategies for new launches and campaigns in collaboration with cross-functional marketing teams
-Contribute to our vision for how the AI-powered Strata Network Security Platform is essential in securing customers' networks, both at internal and external events
-Work closely with Product Management and Product Marketing peers to build a unified market and product vision, developing value propositions and messaging that resonate with our target buyer personas and form the foundation for all product marketing activities
-Serve as the primary thought leader for the products you support, engaging in speaking opportunities and content development
-Simplify product capabilities into clear, compelling messages and tools that resonate with customers
-Create powerful marketing content, including datasheets, whitepapers, blogs, and presentations
-Educate and inspire our Sales, GTM, and field marketing teams on the customer and business value of our products
+...
 """
 
 job_tasks_input = st.text_area("Enter Job Tasks (one per line):", height=350, value=default_job_tasks)
